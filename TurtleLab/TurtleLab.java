6f905cebd854ebdfe049d453d6bdde894c31913b
Nicolas Mills <millspec98@yahoo.com>
2014-09-09 12:19:21 -0500

    Turtle Lab final rev 9/9/2014
    Final revision

--- TurtleLab/TurtleLab.java
+++ TurtleLab/TurtleLab.java
@@ -9,32 +9,25 @@ public class TurtleLab
 {
     public static void main(String[] args)
     {
-        World turtleWorld = new World();
+        World turtleWorld = new World(1720,1080);
         Turtle t1 = new Turtle(turtleWorld);
         Turtle t2 = new Turtle(turtleWorld);
-        //set counter variables
+        t1.hide();
+        t2.hide();
+        //set counter/random variables
         int travel_distance = 30;
         Random generator = new Random();
         int rand_num = generator.nextInt(5);
         int rand_num2 = rand_num;
-        int color_change = generator.nextInt(3);
-        //Turtle1 methods
-        //Set Turtle attributes
-        t1.setShellColor(Color.BLACK);
-        t1.setBodyColor(Color.BLUE);
-        t1.setPenColor(Color.RED);
-        t1.setPenWidth(2);
-        t1.penUp();
-        t1.forward(50);
-        t1.penDown();
+        int atr_change = generator.nextInt(6+2);
+        int it_count = 0;
+        
 
-       
-
-        // 1st Pattern While Loop
+        //While Loop
         while (rand_num2 != -101)
         {
             try {
-                TimeUnit.MILLISECONDS.sleep(50);
+                TimeUnit.MILLISECONDS.sleep(1);
             } catch (InterruptedException e) {
             }
             //t1
@@ -42,37 +35,52 @@ public class TurtleLab
             t1.turn(rand_num);
             t1.forward(travel_distance);
             travel_distance += 1;
-            //change colors
-            if (color_change == 0)
+            Color t1_color = new Color(generator.nextInt(255), generator.nextInt(255), generator.nextInt(255));
+            Color t2_color = new Color(generator.nextInt(255), generator.nextInt(255), generator.nextInt(255));
+            //change colors Delete Comment Marks in if structures to change pen widths while running
+            if (atr_change == 2)
             {
-                t2.setPenColor(Color.BLUE);
-                t1.setPenColor(Color.RED);
+                t2.setPenColor(t2_color);
+                //t2.setPenWidth(atr_change*15);
+                t1.setPenColor(t1_color);
+                //t1.setPenWidth(atr_change*15);
             }
-            if (color_change == 1)
+            if (atr_change == 3)
             {
-                t2.setPenColor(Color.RED);
-                t1.setPenColor(Color.BLUE);
+                t2.setPenColor(t2_color);
+                //t2.setPenWidth(atr_change*15);
+                t1.setPenColor(t1_color);
+                //t1.setPenWidth(atr_change*15);
             }
-            if (color_change == 2)
+            if (atr_change == 4)
             {
-                t2.setPenColor(Color.GREEN);
-                t1.setPenColor(Color.BLACK);
+                t2.setPenColor(t2_color);
+                //t2.setPenWidth(atr_change*15);
+                t1.setPenColor(t1_color);
+                //t1.setPenWidth(atr_change*15);
             }
-            if (color_change == 3)
+            if (atr_change == 5)
             {
-                t2.setPenColor(Color.BLACK);
-                t1.setPenColor(Color.GREEN);
+                t2.setPenColor(t2_color);
+                //t2.setPenWidth(atr_change*15);
+                t1.setPenColor(t1_color);
+                //t1.setPenWidth(atr_change*15);
             }
-            if (color_change == 4)
+            if (atr_change == 6)
             {
-                t2.setPenColor(Color.YELLOW);
-                t1.setPenColor(Color.ORANGE);
+                t2.setPenColor(t2_color);
+                //t2.setPenWidth(atr_change*15);
+                t1.setPenColor(t1_color);
+                //t1.setPenWidth(atr_change*15);
             }
-            if (color_change == 5)
+            if (atr_change == 7)
             {
-                t2.setPenColor(Color.ORANGE);
-                t1.setPenColor(Color.YELLOW);
+                t2.setPenColor(t2_color);
+                //t2.setPenWidth(atr_change*15);
+                t1.setPenColor(t1_color);
+                //t1.setPenWidth(atr_change*15);
             }
+            
             //t2
             rand_num2 = generator.nextInt(360);
             t2.turn(rand_num2 *= -1);
@@ -81,8 +89,14 @@ public class TurtleLab
             {
                 travel_distance *= .625;
             }
-            color_change = generator.nextInt(6);
+            atr_change = generator.nextInt(6);
+            it_count += 1;
+            if ((rand_num2 == -101)&&(it_count < 5000)) //change comparison number to force program to run longer or shorter
+            {
+                rand_num2 = -100;
+            }
         }  
+        System.out.println("This program executed a total of "+it_count+" iterations"); 
     }
 }
 
